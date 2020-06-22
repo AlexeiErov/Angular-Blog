7d31647ca0869ee171a3a583780e0ea9813d0436
Alexey Erov <alexxey.erov@gmail.com>
2020-06-22 19:16:51 +0300

    Project added

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import {Environment} from "./interface";
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyDcfWrECQ8G5lj4-ID9d4MYZv2vvCMfOQk',
+  fbDbUrl: 'https://angular-blog-59387.firebaseio.com'
+};
